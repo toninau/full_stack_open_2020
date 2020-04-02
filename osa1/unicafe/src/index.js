1c72164f8c653bb83388e4c8664049fc79457dbc
Toni Naumanen <toni.naumis@gmail.com>
2020-04-02 14:50:59 +0300

    1.8 valmis

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Display = ({ good, neutral, bad }) => {
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
-
   const average = () => {
     if (all > 0) {
       return (good - bad) / all
     }
     return 0
   }
-
   const positive = () => {
     if (all > 0) {
       return (good / all) * 100
@@ -44,27 +42,23 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const increaseGood = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button
-        handleClick={increaseGood}
+        handleClick={() => setGood(good + 1)}
         text='Good'
       />
       <Button
-        handleClick={increaseNeutral}
+        handleClick={() => setNeutral(neutral + 1)}
         text='Neutral'
       />
       <Button
-        handleClick={increaseBad}
+        handleClick={() => setBad(bad + 1)}
         text='Bad'
       />
       <h1>statistics</h1>
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
