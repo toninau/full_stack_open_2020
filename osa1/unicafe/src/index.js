9e8e5c243a46242e5eb4ea7f3eb21f9453f7fe4e
Toni Naumanen <toni.naumis@gmail.com>
2020-04-02 13:39:08 +0300

    1.6 valmis

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Display = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const increaseGood = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button
+        handleClick={increaseGood}
+        text='Good'
+      />
+      <Button
+        handleClick={increaseNeutral}
+        text='Neutral'
+      />
+      <Button
+        handleClick={increaseBad}
+        text='Bad'
+      />
+      <h1>statistics</h1>
+      <Display good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
