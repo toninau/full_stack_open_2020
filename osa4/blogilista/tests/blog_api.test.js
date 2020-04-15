e0a87d911edc101db5ce0f27fb82386ff9129b03
Toni Naumanen <toni.naumis@gmail.com>
2020-04-15 15:35:17 +0300

    4.10 valmis

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,41 +1,20 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  }
-]
 
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+  await Blog.insertMany(helper.initialBlogs)
 })
 
 describe('HTTP GET /api/blogs', () => {
   test('right amount of blogs is returned in JSON format', async () => {
     const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
-    expect(response.body).toHaveLength(initialBlogs.length)
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
   })
   test('identifying field is named id instead of _id', async () => {
     const response = await api.get('/api/blogs').expect(200).expect('Content-Type', /application\/json/)
@@ -44,6 +23,39 @@ describe('HTTP GET /api/blogs', () => {
     })
   })
 })
+
+describe('HTTP POST /api/blogs', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      _id: '5a422bc61b54a676234d17fc',
+      title: 'Type wars',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+      likes: 2,
+      __v: 0
+    }
+
+    const newBlogReturned = {
+      id: newBlog._id,
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+      likes: newBlog.likes
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    expect(blogsAtEnd).toContainEqual(newBlogReturned)
+  })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
