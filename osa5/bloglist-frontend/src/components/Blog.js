8ce3ff43fc898672d95ed247546625b84ed06676
Toni Naumanen <toni.naumis@gmail.com>
2020-04-23 13:44:21 +0300

    5.7 valmis

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  if (!visible) {
+    return (
+      <div style={blogStyle}>
+        <div>{blog.title} {blog.author}
+          <button onClick={toggleVisibility}>view</button>
+        </div>
+      </div>
+    )
+  }
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title} {blog.author}
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+      <p>{blog.url}</p>
+      <div>likes {blog.likes}
+        <button onClick={() => console.log('like')}>like</button>
+      </div>
+      <p>{blog.user.name}</p>
+    </div>
+  )
+}
+
 
 export default Blog
