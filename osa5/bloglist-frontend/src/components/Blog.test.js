67b747aa023fe77baeb351adc31b757dde8faaf9
Toni Naumanen <toni.naumis@gmail.com>
2020-04-27 12:43:57 +0300

    5.14 valmis

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
-test('renders title and author', () => {
-  const blog = {
-    title: 'test_title',
-    author: 'test_author',
-    url: 'test_url',
-    likes: 22
+const blog = {
+  title: 'test_title',
+  author: 'test_author',
+  url: 'test_url',
+  likes: 22,
+  user: {
+    name: 'test_name'
   }
+}
 
+test('renders title and author', () => {
   const component = render(
     <Blog
       blog={blog}
@@ -24,4 +27,23 @@ test('renders title and author', () => {
   expect(component.container).toHaveTextContent('test_author')
   expect(component.container).not.toHaveTextContent('test_url')
   expect(component.container).not.toHaveTextContent(22)
+})
+
+test('clicking the button shows url and likes', () => {
+  const component = render(
+    <Blog
+      blog={blog}
+      handleLike={() => console.log('like')}
+      handleRemove={() => console.log('remove')}
+      username={'test_username'}
+    />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent('test_title')
+  expect(component.container).toHaveTextContent('test_author')
+  expect(component.container).toHaveTextContent('test_url')
+  expect(component.container).toHaveTextContent(22)
 })
