77c83b8aedfba094a9ac952d6c8b2beb2839cd75
Toni Naumanen <toni.naumis@gmail.com>
2020-05-14 13:48:55 +0300

    8.4 valmis

--- osa8/library-backend/library-backend.js
+++ osa8/library-backend/library-backend.js
@@ -95,14 +95,14 @@ const typeDefs = gql`
     title: String!
     published: Int!
     author: String!
-    genres: [String]!
+    genres: [String!]!
     id: ID!
   }
 
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -111,7 +111,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books
+      }
+       return books.filter(book => book.author === args.author)
+    },
     allAuthors: () => authors
   },
   Author: {
