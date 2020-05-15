a45497102d68ca1d76d60aacc746abe853ffe371
Toni Naumanen <toni.naumis@gmail.com>
2020-05-15 14:36:17 +0300

    8.9 valmis

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,11 +1,26 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+    }
+  }
+`
 
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
 
   return (
     <div>
@@ -22,7 +37,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {result.data.allBooks.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
