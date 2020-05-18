6bf27a36feaadf2a13e0a89dd629bda533a406ec
Toni Naumanen <toni.naumis@gmail.com>
2020-05-18 17:41:32 +0300

    8.19 valmis

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [books, setBooks] = useState([])
+  const [filteredBooks, setFilteredBooks] = useState([])
+  const [genres, setGenres] = useState([])
+
+  useEffect(() => {
+    setBooks(result.loading ? [] : result.data.allBooks)
+  }, [result])
+
+  useEffect(() => {
+    const genreArrays = books.map(book => book.genres)
+    setGenres([...new Set(genreArrays.flat())])
+    setFilteredBooks(books)
+  }, [books])
 
   if (!props.show) {
     return null
   }
 
-  if (result.loading) {
-    return <div>loading...</div>
+  const filter = (genre) => {
+    if (!genre) {
+      setFilteredBooks(books)
+    } else {
+      setFilteredBooks(books.filter(book => book.genres.includes(genre)))
+    }
   }
 
   return (
@@ -28,15 +45,21 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {result.data.allBooks.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {filteredBooks.map(book =>
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           )}
         </tbody>
       </table>
+      <div>
+        {genres.map(genre =>
+          <button key={genre} onClick={() => filter(genre)}>{genre}</button>
+        )}
+        <button onClick={() => filter(null)}>all genres</button>
+      </div>
     </div>
   )
 }
