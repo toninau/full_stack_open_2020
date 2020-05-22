13d6e17c8e6ea210f9d1c22cd9f092fed87f9c50
Toni Naumanen <toni.naumis@gmail.com>
2020-05-22 12:49:22 +0300

    9.6 valmis

--- osa9/exercises_9.1.-9.7/bmiCalculator.ts
+++ osa9/exercises_9.1.-9.7/bmiCalculator.ts
@@ -11,35 +11,37 @@ const parseArguments = (args: Array<string>): BMIValues => {
     return {
       height: Number(args[2]),
       weight: Number(args[3])
-    }
+    };
   } else {
     throw new Error('Provided values were not numbers!');
   }
-}
+};
 
 export const calculateBmi = (height: number, weight: number): string => {
-  const bmi: number = weight / ((height * height) / 10000)
+  const bmi: number = weight / ((height * height) / 10000);
   if (bmi < 15) {
-    return 'Very severely underweight'
+    return 'Very severely underweight';
   } else if (bmi < 16) {
-    return 'Severely underweight'
+    return 'Severely underweight';
   } else if (bmi < 18.5) {
-    return 'Underweight'
+    return 'Underweight';
   } else if (bmi < 25) {
-    return 'Normal (healthy weight)'
+    return 'Normal (healthy weight)';
   } else if (bmi < 30) {
-    return 'Overweight'
+    return 'Overweight';
   } else if (bmi < 35) {
-    return 'Obese Class I (Moderately obese)'
+    return 'Obese Class I (Moderately obese)';
   } else if (bmi < 40) {
-    return 'Obese Class II (Severely obese)'
+    return 'Obese Class II (Severely obese)';
   }
-  return 'Obese Class III (Very severely obese)'
-}
+  return 'Obese Class III (Very severely obese)';
+};
 
 try {
   const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight))
+  console.log(calculateBmi(height, weight));
 } catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
+  if (e instanceof Error) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
 }
--- osa9/exercises_9.1.-9.7/exerciseCalculator.ts
+++ osa9/exercises_9.1.-9.7/exerciseCalculator.ts
@@ -15,24 +15,24 @@ interface ExerciseValues {
 
 const argumentParser = (args: Array<string>): ExerciseValues => {
   if (args.length < 4) throw new Error('Not enough arguments');
-  const hours = args.slice(3).map(hour => Number(hour))
+  const hours = args.slice(3).map(hour => Number(hour));
   if (!isNaN(Number(args[2])) && !hours.some(isNaN)) {
     return {
       target: Number(args[2]),
       hours
-    }
+    };
   } else {
     throw new Error('Provided values were not numbers!');
   }
-}
+};
 
 const calculateExercises = (dailyHours: number[], targetHour: number): Result => {
-  const descriptions: string[] = ['room for improvement', 'not too bad but could be better', 'excellent']
-  const average: number = dailyHours.reduce((a, b) => a + b) / dailyHours.length
-  const trainingDays: number = dailyHours.filter(hour => hour !== 0).length
-  const success: boolean = average >= targetHour ? true : false
-  const rating: number = success ? 3 : average >= (targetHour * 0.85) ? 2 : 1
-  const ratingDescription: string = descriptions[rating - 1]
+  const descriptions: string[] = ['room for improvement', 'not too bad but could be better', 'excellent'];
+  const average: number = dailyHours.reduce((a, b) => a + b) / dailyHours.length;
+  const trainingDays: number = dailyHours.filter(hour => hour !== 0).length;
+  const success: boolean = average >= targetHour ? true : false;
+  const rating: number = success ? 3 : average >= (targetHour * 0.85) ? 2 : 1;
+  const ratingDescription: string = descriptions[rating - 1];
   const result: Result = {
     periodLength: dailyHours.length,
     trainingDays,
@@ -41,13 +41,15 @@ const calculateExercises = (dailyHours: number[], targetHour: number): Result =>
     ratingDescription,
     target: targetHour,
     average
-  }
-  return result
-}
+  };
+  return result;
+};
 
 try {
   const { hours, target } = argumentParser(process.argv);
-  console.log(calculateExercises(hours, target))
+  console.log(calculateExercises(hours, target));
 } catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
+  if (e instanceof Error) {
+    console.log('Error, something bad happened, message: ', e.message);
+  }
 }
