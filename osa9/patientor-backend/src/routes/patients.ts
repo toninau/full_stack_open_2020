198be49111cc2616f94988694f112c34ceee4c7f
Toni Naumanen <toni.naumis@gmail.com>
2020-05-23 14:54:46 +0300

    9.11 valmis

---
+++ osa9/patientor-backend/src/routes/patients.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import patientService from '../services/patientService';
+
+const router = express.Router();
+
+router.get('/', (_req, res) => {
+  res.send(patientService.getNonSensitiveEntries());
+});
+
+router.post('/', (_req, res) => {
+  res.send('Saving a diagnose!');
+});
+
+export default router;
+
---
+++ osa9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,27 @@
+import patients from '../../data/patients';
+
+import { Patient, NonSensitivePatient } from '../types';
+
+const getEntries = (): Array<Patient> => {
+  return patients;
+};
+
+const getNonSensitiveEntries = (): NonSensitivePatient[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id,
+    name,
+    dateOfBirth,
+    gender,
+    occupation
+  }));
+};
+
+const addEntry = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addEntry,
+  getNonSensitiveEntries
+};
