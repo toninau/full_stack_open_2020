d44397c473fa32b28fbab9efaf15085f7af8d760
Toni Naumanen <toni.naumis@gmail.com>
2020-05-23 13:33:13 +0300

    9.9 valmis

--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => { 
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
-  
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
